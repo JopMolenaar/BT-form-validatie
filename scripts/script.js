898b2dc537ccae8e1ef0c4a01d0633afee9261d1
Jop <jopmolenaar@icloud.com>
2024-03-06 15:45:04 +0100

    form validation

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,17 @@
+const rootStyles = getComputedStyle(document.documentElement);
+const supportsSelectorWhere =
+    rootStyles.getPropertyValue("--supports-selector-has").trim() == "1";
+
+console.log(
+    "falback js function when :has() isn's supported runs = ",
+    !supportsSelectorWhere
+); // boolean -> true if supported
+if (!supportsSelectorWhere) {
+    displayFollowUpQuestion();
+}
+
+function displayFollowUpQuestion() {
+    console.log("displayFollowUpQuestion");
+}
+
 // TODO scriptje om dingen te showen als progressive enhancement voor als :has() niet werkt

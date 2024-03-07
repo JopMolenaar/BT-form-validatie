740eee5e90365be35d4c3becd0dd75740710ef85
Jop <jopmolenaar@icloud.com>
2024-03-07 13:02:51 +0100

    js instead of has

--- scripts/script.js
+++ scripts/script.js
@@ -61,10 +61,108 @@ function displayFollowUpQuestion() {
     });
 }
 
-// TODO scriptje om error message te showen als progressive enhancement voor als :has() niet werkt
+// function showErrorMessage() {
+//     const everyInput = document.querySelectorAll(
+//         "main form section fieldset label input"
+//     );
+//     everyInput.forEach((input) => {
+//         input.addEventListener("blur", () => {
+//             const label = input.parentElement;
+//             const invalidInput = label.querySelector(
+//                 "input:user-invalid:not(:focus):not(:placeholder-shown)"
+//             );
+//             if (invalidInput) {
+//                 label.classList.add("showErrorMessage");
+//             }
+//         });
+//     });
+// }
+// showErrorMessage();
 
-// TODO scriptje om de links een kleur te geven als een fieldset invalid is of valid is of een error heeft
+// scriptje om error message te showen als progressive enhancement voor als :has() niet werkt
+// scriptje om de links een kleur te geven als een fieldset invalid is of valid is of een error heeft
 
+function colorLinkNav(sectionId) {
+    if(sectionId === undefined){
+        sectionId = window.location.hash;
+    }
+    const everySection = document.querySelectorAll("main form section");
+    everySection.forEach((section) => {
+        const linkMatchedSection = document.querySelector(
+            `main nav a[href='#${section.id}']`
+        );
+        if(sectionId === '#' + section.id){
+            linkMatchedSection.style.background = "blue";
+            const theOtherLinks = document.querySelectorAll(
+                `main nav a:not([href='#${section.id}'])`
+            );
+            theOtherLinks.forEach((link)=>{
+                // TODO check if section with the fieldsets are valid
+                checkIfSectionsAreValid(link)
+            })
+        } 
+    
+    });
+}
+colorLinkNav();
+
+function  checkIfSectionsAreValid(link) {
+    let currentUrl = link.href;
+    let hashIndex = currentUrl.indexOf("#");
+    let hashAndNext = currentUrl.substring(hashIndex);
+    const everySection = document.querySelectorAll("main form section");
+    everySection.forEach((section) => {
+        if('#' + section.id === hashAndNext){
+            const inputError = section.querySelector("fieldset label input:user-invalid:not(:focus):not(:placeholder-shown)");
+            const validInputs = section.querySelectorAll("fieldset label input:required:valid:not(:placeholder-shown)");
+            const allInputs = section.querySelectorAll("fieldset input:required");
+            const linkMatchedSection = document.querySelector(
+                `main nav a[href='#${section.id}']`
+            );
+            if(inputError){
+                linkMatchedSection.style.background = "red"
+            } else if (validInputs.length >= allInputs.length) {
+                linkMatchedSection.style.background = "green"
+            } else {
+                linkMatchedSection.style.background = "#8fcae7"
+            }
+        }
+    });
+}
+
+
+function displayErrorMessage() {
+    const everySection = document.querySelectorAll("main form section");
+    everySection.forEach((section) => {
+        const everyInput = section.querySelectorAll("fieldset label input");
+        everyInput.forEach((input) => {
+            input.addEventListener("blur", () => {
+                const label = input.parentElement;
+                const invalidInput = label.querySelector(
+                    "input:user-invalid:not(:focus):not(:placeholder-shown)"
+                );
+                if (invalidInput) {
+                    label.classList.add("showErrorMessage");
+                } else {
+                    label.classList.remove("showErrorMessage");
+                }
+            });
+        });
+    });
+}
+displayErrorMessage(); 
+
+const directionButtons = document.querySelectorAll(
+    "main form > section > div > a"
+);
+directionButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        let currentUrl = button.href;
+        let hashIndex = currentUrl.indexOf("#");
+        let hashAndNext = currentUrl.substring(hashIndex);
+        colorLinkNav(hashAndNext);
+    });
+});
 // scriptje zodat de datum in de toekomst bij datum velden niet gekozen kan worden
 const inputDisabledFutures = [
     "input[name=overlijdensdatum-overledene]",
